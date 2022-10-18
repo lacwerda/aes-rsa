23a6cf8c5b1cc3e836cc93bffb3348b73ee6f0e5
Duda <eduarda.lacerda@nubank.com.br>
2022-10-18 00:08:57 -0300

    rsa encryption

---
+++ beatriz.py
@@ -0,0 +1,27 @@
+from components.aes import Aes
+from components.rsa import Rsa
+
+def main():
+    mensagem = open("files/test.txt", "r").read()
+
+    bia = Rsa()
+
+    mensagem_cifrada = letra = bia.OAEPEncryption(bia.publicKey, bytes(mensagem, 'utf-8'))
+
+    """
+    Leitura dos arquivos
+    """
+    # with open("files/mensagem_encrypted.txt", "rb") as arquivo:
+    #     mensagem_cifrada = base64.b64decode(arquivo.read())
+
+    # with open("files/nonce.txt", "r") as arquivo:
+    #     nonce = int(arquivo.read())
+
+    mensagem_final = bia.OAEPDecryption(bia.privateKey, mensagem_cifrada).decode("utf-8")
+
+    assert mensagem_final == mensagem
+
+    print(mensagem_final)
+
+if __name__ == '__main__': 
+    main()
--- bia.py
+++ bia.py
@@ -5,18 +5,18 @@ import base64
 def main():
     mensagem = open("files/test.txt", "r").read()
 
-    aesObject1, aesObject2 = Aes(), Aes()
+    bia, samuel = Aes(), Aes()
 
-    cifraDaMensagem, numberOnce = aesObject1.CtrEncryption(mensagem)
+    cifra_mensagem, number_once = samuel.CtrEncryption(mensagem)
     
     """
     Escrita dos arquivos.
     """
     with open("files/nonce.txt", "w") as arquivo:
-        arquivo.write(str(numberOnce))
+        arquivo.write(str(number_once))
 
     with open("files/mensagem_encrypted.txt", "wb") as arquivo:
-        arquivo.write(base64.b64encode(cifraDaMensagem))
+        arquivo.write(base64.b64encode(cifra_mensagem))
 
     """
     Leitura dos arquivos
@@ -27,11 +27,9 @@ def main():
     with open("files/nonce.txt", "r") as arquivo:
         nonce = int(arquivo.read())
 
-    aesObject2 = Aes()
+    mensagem_final = bia.CtrDecryption(g, nonce)
 
-    mensagemFinal = aesObject2.CtrDecryption(g, nonce)
-
-    print(mensagemFinal)
+    print(mensagem_final)
 
 if __name__ == '__main__': 
     main()
